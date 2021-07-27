805ef0cacbd10608725da9b5ce15c77516966fa0
metasight <56424616+cello4u@users.noreply.github.com>
2021-07-27 15:00:41 +0900

    Create 미션2_Q4_버스요금계산기2.py

---
+++ 미션2_Q4_버스요금계산기2.py
@@ -0,0 +1,53 @@
+#Q4. 버스요금 계산기2-재귀하기
+def bus_fare(age,method):
+        # 입력값 int 변환
+        try:
+                int_age=int(input("나이를 입력하세요(숫자로만 입력합니다): "))
+                int_method=int(input("지불 방법을 택하세요(현금=0, 카드=1): "))
+        except:
+                print("숫자로만 입력해야 합니다. 다시 입력해주세요.")
+                bus_fare(age,method)
+        
+        # 버스요금 계산
+        if int_method == 0:
+                str_method = "현금"
+                if int_age < 8:
+                        str_method = "무료"
+                        fare = "무료"
+                elif int_age >= 8 and int_age < 14:
+                        fare = "450원"
+                elif int_age >= 14 and int_age < 20:
+                        fare = "1,000원"
+                elif int_age >= 20 and int_age < 75:
+                        fare = "1,300원"
+                else :
+                        str_method = "무료"
+                        fare = "무료"
+        elif int_method == 1:
+                str_method = "카드"
+                if int_age < 8:
+                        str_method = "무료"
+                        fare = "무료"
+                elif int_age >= 8 and int_age < 14:
+                        fare = "450원"
+                elif int_age >= 14 and int_age < 20:
+                        fare = "720원"
+                elif int_age >= 20 and int_age < 75:
+                        fare = "1,200원"
+                else :
+                        str_method = "무료"
+                        fare = "무료"
+        else:
+                print("지불유형 입력이 잘못되었습니다. 다시 입력해주세요.")
+                bus_fare(age,method)
+        
+        #결과 출력
+        print("요금 계산결과는 아래와 같습니다.")
+        print("나이: ",int_age,"세")
+        print("지불유형: ",str_method)
+        print("버스요금: ",fare)
+
+print("버스요금 계산기")
+age=0
+method=0
+bus_fare(age,method)
